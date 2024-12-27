1b8ea661a03b488069cd01ceeada8656e2f90c55
Cedric Bouwen <cedric.bouwen@hotmail.com>
2024-12-27 15:58:47 +0100

    Working pong url

--- transcendence/pong/urls.py
+++ transcendence/pong/urls.py
@@ -4,5 +4,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('pong/', views.pong, name='pong'),
+    path('', views.pong, name='pong'),
 ]
