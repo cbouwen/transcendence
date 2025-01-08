b674ae9523ac89fe0d3e89a81814ee4d520f6e42
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2025-01-08 19:47:39 +0100

    working on tetris game

--- tetris/models.py
+++ tetris/models.py
@@ -5,7 +5,7 @@ class TetrisScore(models.Model):
     """
     Model to store Tetris game results.
     """
-    gameid = models.CharField(max_length=100)  # Unique or shared ID representing a game instance
+    gameid = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     score = models.IntegerField()
     lines_cleared = models.IntegerField()
@@ -14,3 +14,15 @@ class TetrisScore(models.Model):
 
     def __str__(self):
         return f"Game {self.gameid} - {self.name}: {self.score}"
+
+
+class TetrisPlayer(models.Model):
+    """
+    Model to store Tetris player data.
+    """
+    player_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    matchmaking_rating = models.IntegerField()
+
+    def __str__(self):
+        return f"Player {self.player_id} - {self.name}: {self.matchmaking_rating}"
