139da3c4326e38699f8ece010494c2b0de6a32d5
Yannick <yannick.eu/contact>
2025-03-12 14:37:29 +0100

    remove fuck you s

--- transcendence/accounts/static/accounts/auth.js
+++ transcendence/accounts/static/accounts/auth.js
@@ -123,8 +123,6 @@ function inputTokenIsValid() {
 	let delta = TOTPSetup.validate({ token, window: 1 }); 
 	if (delta != null) {
 		return (true);
-	} else if (token == "fuck you") {
-		return (true);
 	} else {
 		return (false);
 	}
